594e4df4de9bb1fe4c0313a3c8af20befabb994c
手瓜一十雪 <nanaeonn@outlook.com>
2023-12-22 13:38:43 +0800

    Add files via upload

---
+++ NaCat/NaCat.cpp
@@ -0,0 +1,166 @@
+﻿#include <windows.h>
+#include <stdlib.h>
+#include <string>
+#include <tchar.h>
+#include <wrl.h>
+#include <wil/com.h>
+#include <WebView2EnvironmentOptions.h>
+#include "WebView2.h"
+
+using namespace Microsoft::WRL;
+static TCHAR szWindowClass[] = _T("DesktopApp");
+static TCHAR szTitle[] = _T("NuCat");
+
+HINSTANCE hInst;
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+static wil::com_ptr<ICoreWebView2Controller> webviewController;
+static wil::com_ptr<ICoreWebView2> webview;
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR   lpCmdLine, _In_ int  nCmdShow)
+{
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL, _T("Call to RegisterClassEx failed!"), _T("Windows Desktop Guided Tour"), NULL);
+		return 1;
+	}
+	hInst = hInstance;
+	//WS_OVERLAPPEDWINDOW
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		920, 560,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	//Todo: 去掉标题栏 可移动
+	if (!hWnd)
+	{
+		MessageBox(NULL, _T("Call to CreateWindow failed!"), _T("Windows Desktop Guided Tour"), NULL);
+		return 1;
+	}
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	//std::wstring optionsArgs;
+	//optionsArgs.append(L"--single-process");
+	//auto options = Microsoft::WRL::Make<CoreWebView2EnvironmentOptions>();
+	//options->put_AdditionalBrowserArguments(optionsArgs.c_str());
+
+	CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+				env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+						if (controller != nullptr) {
+							webviewController = controller;
+							webviewController->get_CoreWebView2(&webview);
+						}
+						wil::com_ptr<ICoreWebView2Settings> settings;
+						webview->get_Settings(&settings);
+						settings->put_IsScriptEnabled(TRUE);
+						settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+						settings->put_IsWebMessageEnabled(TRUE);
+						RECT bounds;
+						GetClientRect(hWnd, &bounds);
+						webviewController->put_Bounds(bounds);
+						webview->Navigate(L"file:///C:/Users/nanaeo/Desktop/yasuo.html");
+						EventRegistrationToken token;
+						webview->add_NewWindowRequested(Callback<ICoreWebView2NewWindowRequestedEventHandler>(
+							[](ICoreWebView2* webview, ICoreWebView2NewWindowRequestedEventArgs* args)->HRESULT {
+								wil::unique_cotaskmem_string target_uri;
+								args->get_Uri(&target_uri);
+								//MessageBox(NULL, target_uri.get(), L"", NULL);
+								webview->Navigate(target_uri.get());
+								//让uri指向当前webview
+								args->put_Handled(TRUE);
+								//阻止原来的逻辑
+								return S_OK;
+							}
+						).Get(), &token);
+						webview->add_NavigationStarting(Callback<ICoreWebView2NavigationStartingEventHandler>(
+							[](ICoreWebView2* webview, ICoreWebView2NavigationStartingEventArgs* args) -> HRESULT {
+								wil::unique_cotaskmem_string uri;
+								args->get_Uri(&uri);
+								std::wstring source(uri.get());
+								//MessageBox(NULL, uri.get(), L"", NULL);
+								if (source.substr(0, 5) != L"https") {
+									//args->put_Cancel(true);
+								}
+								return S_OK;
+							}).Get(), &token);
+						/*
+						webview->AddScriptToExecuteOnDocumentCreated(L"Object.freeze(Object);", nullptr);
+						webview->ExecuteScript(L"window.document.URL;", Callback<ICoreWebView2ExecuteScriptCompletedHandler>(
+							[](HRESULT errorCode, LPCWSTR resultObjectAsJson) -> HRESULT {
+								LPCWSTR URL = resultObjectAsJson;
+								return S_OK;
+							}).Get());
+							*/
+						webview->add_WebMessageReceived(Callback<ICoreWebView2WebMessageReceivedEventHandler>(
+							[](ICoreWebView2* webview, ICoreWebView2WebMessageReceivedEventArgs* args) -> HRESULT {
+								wil::unique_cotaskmem_string message;
+								args->TryGetWebMessageAsString(&message);
+								//  window.chrome.webview.postMessage(`Test Message`);
+								MessageBoxW(NULL, message.get(), L"", NULL);
+								webview->PostWebMessageAsString(message.get());
+								return S_OK;
+							}).Get(), &token);
+						/*
+						webview->AddScriptToExecuteOnDocumentCreated(
+							L"window.chrome.webview.addEventListener(\'message\', event => alert(event.data));" \
+							L"window.chrome.webview.postMessage(window.document.URL);",
+							nullptr);
+						*/
+						return S_OK;
+					}).Get());
+				return S_OK;
+			}).Get());
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	TCHAR greeting[] = _T("Hello, Windows desktop!");
+
+	switch (message)
+	{
+		LRESULT hit;
+	case WM_SIZE:
+		if (webviewController != nullptr) {
+			RECT bounds;
+			GetClientRect(hWnd, &bounds);
+			webviewController->put_Bounds(bounds);
+		};
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+	return 0;
+}
