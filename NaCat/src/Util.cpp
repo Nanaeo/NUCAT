458611c66bb5bd2cd343c1ae04b1bf94ffa4a956
手瓜一十雪 <nanaeonn@outlook.com>
2023-12-22 21:11:12 +0800

    Update Util.cpp

--- NaCat/src/Util.cpp
+++ NaCat/src/Util.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-#include <filesystem>
+#include <Windows.h>
+#include <string>
+
+std::wstring ResolvePathAndTrimFile(std::wstring path)
+{
+    std::wstring::size_type pos = path.find_last_of(L"\\/");
+    if (pos != std::wstring::npos)
+    {
+        return path.substr(0, pos);
+    }
+    return path;
+}
+
+std::wstring GetCurrentPath()
+{
+    wchar_t appPath[MAX_PATH];
+    GetModuleFileNameW(nullptr, appPath, ARRAYSIZE(appPath));
+    return ResolvePathAndTrimFile(appPath);
+}
+
 std::wstring GetResourceEntry() {
-	std::wstring path = std::filesystem::current_path().wstring();
+	std::wstring path = GetCurrentPath();
 	std::wstring file = L"Resource\\index.html";
 	std::wstring full_path = L"file:\\\\\\" + path + L"\\" + file;
 	return full_path;
