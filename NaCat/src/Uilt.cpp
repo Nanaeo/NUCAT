d365b2cc5659713bff2edc094d874d3bb0264606
手瓜一十雪 <nanaeonn@outlook.com>
2023-12-22 19:44:10 +0800

    Delete NaCat/src/Uilt.cpp

--- NaCat/src/Uilt.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <filesystem>
-std::wstring GetResourceEntry() {
-	std::wstring path = std::filesystem::current_path().wstring();
-	std::wstring file = L"Resource\\index.html";
-	std::wstring full_path = L"file:\\\\\\" + path + L"\\" + file;
-	return full_path;
-}
