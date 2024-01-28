793455b61145c4de3f32f2c17d5caf74cf8d4304
手瓜一十雪 <nanaeonn@outlook.com>
2024-01-28 14:43:47 +0800

    fix: LogBuildError

--- NuCat/include/Log.h
+++ NuCat/include/Log.h
@@ -6,7 +6,6 @@ namespace Log {
 	{
 		LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_EXIT
 	};
-	extern std::function<bool(std::string, Log::Level, std::string)> HandlerFunction;
 	extern int CurrentLogLevel;
 	void SetLogLevel(int LogLevel);
 	void DeleteAll();
