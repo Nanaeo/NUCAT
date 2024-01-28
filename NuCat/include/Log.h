a6dc030dfe8a51c5feea35b6252f404c3e2557c7
手瓜一十雪 <nanaeonn@outlook.com>
2024-01-28 14:30:53 +0800

    feat: 可拦截日志

--- NuCat/include/Log.h
+++ NuCat/include/Log.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <string>
+#include <functional>
 namespace Log {
 	enum Level
 	{
 		LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_EXIT
 	};
+	extern std::function<bool(std::string, Log::Level, std::string)> HandlerFunction;
 	extern int CurrentLogLevel;
 	void SetLogLevel(int LogLevel);
 	void DeleteAll();
-	void SetOutHandler();
+	void SetOutHandler(std::function<bool(std::string, Log::Level, std::string)> callback);
 	bool Logging(std::string content, Level ErrorLevel);
+	bool DefaultHandler(std::string content, Level ErrorLevel, std::string LogLevelStr);
 }
--- NuCat/src/Log.cpp
+++ NuCat/src/Log.cpp
@@ -1,7 +1,8 @@
 #include "include/Log.h"
 #include "include/Util.h"
 #include "include/FileOperator.h"
-//2024/01/27 等待重构部分
+std::function<bool(std::string, Log::Level, std::string)> HandlerFunction = Log::DefaultHandler;
+
 int Log::CurrentLogLevel = 2; //WARN以上
 void Log::SetLogLevel(int LogLevel)
 {
@@ -10,16 +11,27 @@ void Log::SetLogLevel(int LogLevel)
 void Log::DeleteAll() {
 	// 清除所有日志 直接清空整个日志目录
 }
-void Log::SetOutHandler() {
-	// 设置日志拦截 通过注册函数 让文件out被修改 
+void Log::SetOutHandler(std::function<bool(std::string, Log::Level, std::string)> callback) {
+	HandlerFunction = callback;
+	// 设置日志拦截 通过注册回调函数
 }
 bool Log::Logging(std::string content, Level ErrorLevel)
 {
-	char* LogLevel[] = { (char*)u8"DEBUG", (char*)u8"INFO", (char*)u8"WARN", (char*)u8"ERROR", (char*)u8"EXIT" };
-	std::string CurrentLogLevel = LogLevel[static_cast<int>(ErrorLevel)];
+	if (static_cast<int>(ErrorLevel) < Level::LOG_DEBUG || static_cast<int>(ErrorLevel) > Level::LOG_EXIT) {
+		return false;
+	}
+	// 过滤无效等级日志
 	if (static_cast<int>(ErrorLevel) < Log::CurrentLogLevel) {
 		return false;
 	}
+	// 过滤低等级日志
+	char* LogLevel[] = { (char*)u8"DEBUG", (char*)u8"INFO", (char*)u8"WARN", (char*)u8"ERROR", (char*)u8"EXIT" };
+	std::string LogLevelStr = LogLevel[static_cast<int>(ErrorLevel)];
+	return HandlerFunction(content, ErrorLevel, LogLevelStr);
+}
+
+bool Log::DefaultHandler(std::string Content, Level ErrorLevel, std::string LogLevelStr)
+{
 	std::wstring CurrentDate = Utf8ToUtf16(getCurrentDate().c_str());
 	std::wstring path = L"\\Log\\" + CurrentDate + L".txt";
 	FileOperator LoggFile(GetResourcePath(path.c_str()).c_str());
@@ -29,9 +41,9 @@ bool Log::Logging(std::string content, Level ErrorLevel)
 	RealConten.append(CurrentTimestamp);
 	RealConten.append((char*)u8"] ");
 	RealConten.append((char*)u8"[");
-	RealConten.append(CurrentLogLevel);
+	RealConten.append(LogLevelStr);
 	RealConten.append((char*)u8"] ");
-	RealConten.append(content);
+	RealConten.append(Content);
 	RealConten.append("\n");
 	LoggFile.append(RealConten.c_str());
 	return true;
